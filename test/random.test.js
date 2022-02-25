b89370c63d1962dcd69eb34a831afa9950c3eb06
ADISAKBOONMARK <adisak0661013@gmail.com>
2022-02-25 17:18:51 +0700

    Add fail case

--- test/random.test.js
+++ test/random.test.js
@@ -3,12 +3,17 @@ const { ethers } = require("hardhat");
 const crypto = require("crypto");
 
 describe("Pseudo Radom Number Generator", function () {
-  it("Random and Proving", async function () {
-    const PRNG = await ethers.getContractFactory("PRNG");
-    const randomNumberContract = await PRNG.deploy();
-    const accounts = await ethers.getSigners();
-    await randomNumberContract.deployed();
+  let randomNumberContract;
+  let accounts;
 
+  before(async () => {
+    const PRNG = await ethers.getContractFactory("PRNG");
+    randomNumberContract = await PRNG.deploy();
+    accounts = await ethers.getSigners();
+    await randomNumberContract.deployed();
+  });
+
+  it("Random and Proving", async function () {
     const oracleMsg = crypto.randomBytes(256).toString("hex");
     const clientMsg = crypto.randomBytes(256).toString("hex");
 
@@ -39,5 +44,32 @@ describe("Pseudo Radom Number Generator", function () {
     expect(await randomNumberContract.proving(result, s1, s2, length)).to.equal(
       true
     );
+  });
+
+  it("Should not allow length is '0' or '1'", async function () {
+    const oracleMsg = crypto.randomBytes(256).toString("hex");
+    const clientMsg = crypto.randomBytes(256).toString("hex");
+
+    const oracleEntropy = [
+      accounts[0].address,
+      ethers.utils.hashMessage(oracleMsg),
+      await accounts[0].signMessage(oracleMsg),
+    ];
+
+    const clientEntropy = [
+      accounts[1].address,
+      ethers.utils.hashMessage(clientMsg),
+      await accounts[1].signMessage(clientMsg),
+    ];
+
+    let randLength = 0;
+    await expect(
+      randomNumberContract.random(clientEntropy, oracleEntropy, randLength)
+    ).revertedWith("modifier: length '0' or '1' is not allow");
+
+    randLength = 1;
+    await expect(
+      randomNumberContract.random(clientEntropy, oracleEntropy, randLength)
+    ).revertedWith("modifier: length '0' or '1' is not allow");
   });
 });
